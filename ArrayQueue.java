98230f8c541c0f21fdc34d1efeb90a6c6281943f
Pranu Prakash <83160292+pranuprakash@users.noreply.github.com>
2023-02-25 15:15:16 -0500

    Add files via upload

---
+++ ArrayQueue.java
@@ -0,0 +1,164 @@
+import java.util.NoSuchElementException;
+
+/**
+ * Your implementation of an ArrayQueue.
+ *
+ * @author Pranu Prakash
+ * @version 1.0
+ * @userid pprakash49
+ * @GTID 903703245
+ *
+ */
+public class ArrayQueue<T> {
+
+    /*
+     * The initial capacity of the ArrayQueue.
+     *
+     * DO NOT MODIFY THIS VARIABLE.
+     */
+    public static final int INITIAL_CAPACITY = 9;
+
+    /*
+     * Do not add new instance variables or modify existing ones.
+     */
+    private T[] backingArray;
+    private int front;
+    private int size;
+
+    /**
+     * Constructs a new ArrayQueue.
+     */
+    public ArrayQueue() {
+        backingArray = (T[]) new Object[INITIAL_CAPACITY];
+    }
+
+    /**
+     * Adds the data to the back of the queue.
+     *
+     * If sufficient space is not available in the backing array, resize it to
+     * double the current length. When resizing, copy elements to the
+     * beginning of the new array and reset front to 0.
+     *
+     * Must be amortized O(1).
+     *
+     * @param data the data to add to the back of the queue
+     * @throws java.lang.IllegalArgumentException if data is null
+     */
+    public void enqueue(T data) {
+        if (data == null) {
+            throw new IllegalArgumentException("Data cannot be added to the queue. Entered data is null.");
+        }
+
+        if ((size + 1) > backingArray.length) { // resize needed
+            T[] newArr = (T[]) new Object[backingArray.length * 2];
+            int count = 0;
+
+            for (int i = front; i < backingArray.length; i++) {
+                newArr[count] = backingArray[i];
+                count++;
+            }
+
+            for (int j = 0; j < front; j++) {
+                newArr[count] = backingArray[j];
+                count++;
+            }
+
+            newArr[++count] = data; // ++count or count
+            front = 0;
+
+            backingArray = newArr;
+            size++;
+
+        } else {
+            int index = (front + size) % backingArray.length;
+            // front = (front + 1) % backingArray.length;
+
+            backingArray[index] = data;
+            size++;
+        }
+    }
+
+    /**
+     * Removes and returns the data from the front of the queue.
+     *
+     * Do not shrink the backing array.
+     *
+     * Replace any spots that you dequeue from with null.
+     *
+     * If the queue becomes empty as a result of this call, do not reset
+     * front to 0.
+     *
+     * Must be O(1).
+     *
+     * @return the data formerly located at the front of the queue
+     * @throws java.util.NoSuchElementException if the queue is empty
+     */
+    public T dequeue() {
+        if (size == 0) {
+            throw new NoSuchElementException("Queue is empty. Cannot remove from an empty queue");
+        }
+
+        T returnData = backingArray[front];
+
+        backingArray[front] = null;
+        front = (front + 1) % backingArray.length;
+        size--;
+
+        return returnData;
+    }
+
+    /**
+     * Returns the data from the front of the queue without removing it.
+     *
+     * Must be O(1).
+     *
+     * @return the data located at the front of the queue
+     * @throws java.util.NoSuchElementException if the queue is empty
+     */
+    public T peek() {
+        if (size == 0) {
+            throw new NoSuchElementException("Queue is empty. Cannot peek an empty queue");
+        }
+
+        return backingArray[front];
+    }
+
+    /**
+     * Returns the backing array of the queue.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the backing array of the queue
+     */
+    public T[] getBackingArray() {
+        // DO NOT MODIFY THIS METHOD!
+        return backingArray;
+    }
+
+    /**
+     * Returns the front index of the queue.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the front index of the queue
+     */
+    public int getFront() {
+        // DO NOT MODIFY THIS METHOD!
+        return front;
+    }
+
+    /**
+     * Returns the size of the queue.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the size of the queue
+     */
+    public int size() {
+        // DO NOT MODIFY THIS METHOD!
+        return size;
+    }
+}
